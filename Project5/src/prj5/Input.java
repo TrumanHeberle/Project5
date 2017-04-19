9e957fe713e299a613663f8d7c57a247d3133f86
mashlyn6 <mashlyn6@vt.edu>
2017-04-19 16:17:23 -0400

    Update Input.java

--- Project5/src/prj5/Input.java
+++ Project5/src/prj5/Input.java
@@ -6,6 +6,7 @@ public class Input {
      * @param args the person file and song file in that order
      */
     public static void main(String[] args) {
+        //I think somewhere here we need to create the two different scanners
         GUIDisplayWindow gui;
         gui = new GUIDisplayWindow(args[0], args[1]);
     }

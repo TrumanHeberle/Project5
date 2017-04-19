fc87f3c31dac7c732125e0a3ea1ed463d355c31c
shivani211 <shivanig@vt.edu>
2017-04-19 18:00:29 -0400

    Update PersonScanner.java

--- Project5/src/prj5/PersonScanner.java
+++ Project5/src/prj5/PersonScanner.java
@@ -7,21 +7,36 @@ import java.util.Scanner;
 public class PersonScanner {
     private DoublyLinkedList<Person> peopleList;
     
-    public PersonScanner() {
-        peopleList = new DoublyLinkedList<Person>();
-    }
-    
     public PersonScanner(String fileName) throws FileNotFoundException {
-        this();
+        peopleList = new DoublyLinkedList<Person>();
         this.scanPeople(fileName);
     }
     
     public void scanPeople(String fileName) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(fileName));
         scan.useDelimiter(",");
+        
         while (scan.hasNext()) {
-            System.out.println(scan.next());
+            Scanner line = new Scanner(scan.nextLine());
+            
+            int identification = line.next();
+            String date = line.next();
+            String hobby =  = line.next();
+            String major = line.next();
+            String region =  = line.next();
+            
+            String[] ans = new String[]();
+            int x = 0;
+            while (line.hasNext())
+            {
+                ans[x] = line.next();
+                x++;
+            }
+            
+            Person per = new Person(identification, date, hobby, major, region, ans);
+            peopleList.addToBack(per);
         }
+        
         scan.close();
     }
     

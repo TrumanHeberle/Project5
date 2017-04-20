c6d0f96d035224a3548afd2e29b58a0a11f5cf13
mashlyn6 <mashlyn6@vt.edu>
2017-04-20 00:04:48 -0400

    Update SongTest.java

--- Project5/src/prj5/SongTest.java
+++ Project5/src/prj5/SongTest.java
@@ -30,4 +30,22 @@ public class SongTest extends student.TestCase {
         assertEquals("Pop", song.getGenre());
     }
     
+    /**
+     * test the addData method when the person liked the song
+     */
+    public void testAddData()
+    {
+        String[] answers = new String[6];
+        answers[0] = "yes";
+        answers[1] = "no";
+        answers[2] = "no";
+        answers[3] = "no";
+        answers[4] = "yes";
+        answers[5] = "yes";
+        Person person = new Person(1, "04/10/17", "ART", "MATH_CMDA", "OTHER_US", answers);
+        song.addData(person);
+        assertEquals(song.getTracker().getHeard(), 1);
+        assertEquals(song.getTracker().getLiked(), 0);
+    }
+    
 }

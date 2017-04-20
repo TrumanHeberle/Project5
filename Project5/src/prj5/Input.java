f6c07304487759fce4dbbd2b7c38806416505324
mashlyn6 <mashlyn6@vt.edu>
2017-04-20 14:43:32 -0400

    Update Input.java

--- Project5/src/prj5/Input.java
+++ Project5/src/prj5/Input.java
@@ -1,3 +1,4 @@
+
 package prj5;
 
 import java.io.FileNotFoundException;
@@ -13,41 +14,37 @@ public class Input {
      * @param args the person file and song file in that order
      * @throws FileNotFoundException 
      */
-    public static void main(String[] args) {
-        try {
-            PersonScanner personScanner = new PersonScanner(args[0]);
-        }
-        catch (FileNotFoundException e) {
-            e.getMessage();
-        }
-        SongScanner songScanner = null;
-        try {
-            songScanner = new SongScanner(args[1]);
-        }
-        catch (FileNotFoundException e) {
-            e.getMessage();
-        }
-        GUIDisplayWindow gui;
-        gui = new GUIDisplayWindow();
+    public static void main(String[] args) throws FileNotFoundException {
+        PersonScanner personScanner = new PersonScanner(args[0]);
+        DoublyLinkedList<Person> people = personScanner.getPeopleList();
+        SongScanner songScanner = new SongScanner(args[1]);
         LinkedSongList<Song> songs = songScanner.getSongList().sortByTitle();
+        /**for (Song song: songs) {
+            for (Person person: people) {
+                song.addData(person);
+            }
+        }*/
+        songs.sortByTitle();
         for (int i = 0; i < songs.size(); i++)
         {
             Song currentSong = songs.getEntry(i);
             System.out.println("Song Title: " + currentSong.getTitle());
-            System.out.println("Song Artist: " + currentSong.getGenre());
+            System.out.println("Song Artist: " + currentSong.getArtist());
             System.out.println("Song Genre: " + currentSong.getGenre());
             System.out.println("Song Year: " + currentSong.getYear());
             System.out.println("Heard");
-            System.out.print("reading:" + currentSong.getTracker().getHeardByHobby(HobbyEnum.READ));
-            System.out.print(" art:" + currentSong.getTracker().getHeardByHobby(HobbyEnum.ART));
-            System.out.print(" sports:" + currentSong.getTracker().getHeardByHobby(HobbyEnum.SPORTS));
-            System.out.println(" music:" + currentSong.getTracker().getHeardByHobby(HobbyEnum.MUSIC));
+            System.out.print("reading:" + currentSong.getHobbyScore(HobbyEnum.READ, people)[0]);
+            System.out.print(" art:" + currentSong.getHobbyScore(HobbyEnum.ART, people)[0]);
+            System.out.print(" sports:" + currentSong.getHobbyScore(HobbyEnum.SPORTS, people)[0]);
+            System.out.println(" music:" + currentSong.getHobbyScore(HobbyEnum.MUSIC, people)[0]);
             System.out.println("Likes");
-            System.out.print("reading:" + currentSong.getTracker().getLikedByHobby(HobbyEnum.READ));
-            System.out.print(" art:" + currentSong.getTracker().getLikedByHobby(HobbyEnum.ART));
-            System.out.print(" sports:" + currentSong.getTracker().getLikedByHobby(HobbyEnum.SPORTS));
-            System.out.println(" music:" + currentSong.getTracker().getLikedByHobby(HobbyEnum.MUSIC));
+            System.out.print("reading:" + currentSong.getHobbyScore(HobbyEnum.READ, people)[1]);
+            System.out.print(" art:" + currentSong.getHobbyScore(HobbyEnum.ART, people)[1]);
+            System.out.print(" sports:" + currentSong.getHobbyScore(HobbyEnum.SPORTS, people)[1]);
+            System.out.println(" music:" + currentSong.getHobbyScore(HobbyEnum.MUSIC, people)[1]);
             System.out.println("");
         }
+        GUIDisplayWindow gui;
+        gui = new GUIDisplayWindow();
     }
 }

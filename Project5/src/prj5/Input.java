84b746a6d947393c493492e6270eef92308202a3
mashlyn6 <mashlyn6@vt.edu>
2017-04-19 22:26:17 -0400

    Update Input.java

--- Project5/src/prj5/Input.java
+++ Project5/src/prj5/Input.java
@@ -1,14 +1,32 @@
+/**
+ * 
+ */
 package prj5;
 
+import java.io.FileNotFoundException;
+
+/**
+ * @author AshlynUser
+ *
+ */
 public class Input {
+
     /**
      * Runs the program
      * @param args the person file and song file in that order
+     * @throws FileNotFoundException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         PersonScanner personScanner = new PersonScanner(args[0]);
         SongScanner songScanner = new SongScanner(args[1]);
         GUIDisplayWindow gui;
         gui = new GUIDisplayWindow();
+        LinkedSongList<Song> songs = (LinkedSongList<Song>)new LinkedSongList<Song>().sortByTitle();
+        songs.sortByTitle();
+        for (int i = 0; i < songs.size(); i++)
+        {
+            
+        }
+        System.out.print("hello");
     }
 }

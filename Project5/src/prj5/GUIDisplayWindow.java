635460ef2fec2ab75ce1ff1207622e8850adf9ad
mashlyn6 <mashlyn6@vt.edu>
2017-04-19 21:34:54 -0400

    Update GUIDisplayWindow.java

--- Project5/src/prj5/GUIDisplayWindow.java
+++ Project5/src/prj5/GUIDisplayWindow.java
@@ -1,7 +1,10 @@
 package prj5;
 
+import java.awt.Color;
 //import java.io.FileNotFoundException;
 import CS2114.Button;
+import CS2114.Shape;
+import CS2114.TextShape;
 import CS2114.Window;
 import CS2114.WindowSide;
 
@@ -39,6 +42,7 @@ public class GUIDisplayWindow {
     
     public GUIDisplayWindow() {
         window = new Window();
+        window.setSize(800, 500);
         
         prev = new Button("Prev");
         prev.setTitle("Prev");
@@ -81,6 +85,8 @@ public class GUIDisplayWindow {
         window.addButton(major, WindowSide.SOUTH);
         window.addButton(region, WindowSide.SOUTH);
         window.addButton(quit, WindowSide.SOUTH);
+        createGlyph();
+        createLegend();
         
         /*try {
             personScanner.scanPeople(peopleFile);
@@ -92,6 +98,83 @@ public class GUIDisplayWindow {
         songScanner.scanSongs(songFile);
         songs = songScanner.getSongList();
         songs.sortByTitle();*/
+    }
+    
+    /**
+     * creates a default glyph
+     * 
+     */
+    public void createGlyph()
+    {
+        TextShape title = new TextShape(40, 5, "All These Things I've Done", Color.BLACK);
+        title.setBackgroundColor(Color.WHITE);
+        window.addShape(title);
+        TextShape artist = new TextShape(80, 25, "by The Killers", Color.BLACK);
+        artist.setBackgroundColor(Color.WHITE);
+        window.addShape(artist);
+        Shape line = new Shape(120, 45, 7, 48, Color.BLACK);
+        window.addShape(line);
+        Shape leftPink = new Shape(90, 45, 30, 12, Color.MAGENTA);
+        window.addShape(leftPink);
+        Shape rightPink = new Shape(127, 45, 20, 12, Color.MAGENTA);
+        window.addShape(rightPink);
+        Shape leftBlue = new Shape(70, 57, 50, 12, Color.BLUE);
+        window.addShape(leftBlue);
+        Shape rightBlue = new Shape(127, 57, 40, 12, Color.BLUE);
+        window.addShape(rightBlue);
+        Shape leftYellow = new Shape(80, 69, 40, 12, Color.ORANGE);
+        window.addShape(leftYellow);
+        Shape rightYellow = new Shape(127, 69, 30, 12, Color.ORANGE);
+        window.addShape(rightYellow);
+        Shape leftGreen = new Shape(75, 81, 45, 12, Color.GREEN);
+        window.addShape(leftGreen);
+        Shape rightGreen = new Shape(127, 81, 25, 12, Color.GREEN);
+        window.addShape(rightGreen);
+    }
+    
+    /**
+     * creates the legend
+     */
+    public void createLegend()
+    {
+        Shape box = new Shape(640, 180, 120, 180, Color.BLACK);
+        box.setBackgroundColor(Color.WHITE);
+        window.addShape(box);
+        TextShape hobbyLegend = new TextShape(650, 190, "Hobby Legend", Color.BLACK);
+        hobbyLegend.setBackgroundColor(Color.WHITE);
+        window.addShape(hobbyLegend);
+        window.moveToFront(hobbyLegend);
+        TextShape read = new TextShape(650, 210, "Read", Color.MAGENTA);
+        read.setBackgroundColor(Color.WHITE);
+        window.addShape(read);
+        window.moveToFront(read);
+        TextShape art = new TextShape(650, 230, "Art", Color.BLUE);
+        art.setBackgroundColor(Color.WHITE);
+        window.addShape(art);
+        window.moveToFront(art);
+        TextShape sports = new TextShape(650, 250, "Sports", Color.ORANGE);
+        sports.setBackgroundColor(Color.WHITE);
+        window.addShape(sports);
+        window.moveToFront(sports);
+        TextShape music = new TextShape(650, 270, "Music", Color.GREEN);
+        music.setBackgroundColor(Color.WHITE);
+        window.addShape(music);
+        window.moveToFront(music);
+        TextShape songTitle = new TextShape(665, 290, "Song Title", Color.BLACK);
+        songTitle.setBackgroundColor(Color.WHITE);
+        window.addShape(songTitle);
+        window.moveToFront(songTitle);
+        Shape blackBox = new Shape(700, 310, 5, 44, Color.BLACK);
+        window.addShape(blackBox);
+        window.moveToFront(blackBox);
+        TextShape heard = new TextShape(650, 325, "Heard", Color.BLACK);
+        heard.setBackgroundColor(Color.WHITE);
+        window.addShape(heard);
+        window.moveToFront(heard);
+        TextShape likes = new TextShape(710, 325, "Likes", Color.BLACK);
+        likes.setBackgroundColor(Color.WHITE);
+        window.addShape(likes);
+        window.moveToFront(likes);
     }
     
     public void clickedPrev(Button button) {

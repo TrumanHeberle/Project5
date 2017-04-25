76cfb7c0c4047b9b3fa260dfda51f2d3081228aa
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:55:22 -0400

    Update CompareSongGenreTest.java

--- Project5/src/prj5/CompareSongGenreTest.java
+++ Project5/src/prj5/CompareSongGenreTest.java
@@ -1,43 +1,44 @@
+/**
+ * 
+ */
 package prj5;
 
 /**
- * Tests the CompareSongGenre class
- * 
  * @author AshlynUser
- * @version 4.19.2017
+ * @version 2017.04.19
  */
 public class CompareSongGenreTest extends student.TestCase {
 
     private CompareSongGenre comparer;
-
-
+    
     /**
      * sets up test methods
      */
-    public void setUp() {
+    public void setUp()
+    {
         comparer = new CompareSongGenre();
     }
-
-
+    
     /**
      * tests the compare method when the two objects
      * have different genres
      */
-    public void testCompareDiffGenre() {
+    public void testCompareDiffGenre()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hello", "Michael Jackson", 1997, "rock", 2);
-        assertTrue(comparer.compare(song1, song2) != 0);
+        assertNotSame(0, comparer.compare(song1, song2));
     }
-
-
+    
     /**
      * tests the compare method when the two objects
      * have the same genre
      */
-    public void testCompareSameGenre() {
+    public void testCompareSameGenre()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hello", "Michael Jackson", 1997, "pop", 2);
-        assertTrue(comparer.compare(song1, song2) == 0);
+        assertEquals(0, comparer.compare(song1, song2));
     }
-
+    
 }

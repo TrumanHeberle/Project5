8eeeaa42d7cd0f0a98b8958fc315ce37eb4aa9f0
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:56:01 -0400

    Update CompareSongTitleTest.java

--- Project5/src/prj5/CompareSongTitleTest.java
+++ Project5/src/prj5/CompareSongTitleTest.java
@@ -1,42 +1,44 @@
+/**
+ * 
+ */
 package prj5;
 
 /**
- * Tests the CompareSongTitle class
- * 
  * @author AshlynUser
- * @version 4.19.2017
+ * @version 2017.04.19
  */
 public class CompareSongTitleTest extends student.TestCase {
+
     private CompareSongTitle comparer;
-
-
+    
     /**
      * sets up test methods
      */
-    public void setUp() {
+    public void setUp()
+    {
         comparer = new CompareSongTitle();
     }
-
-
+    
     /**
      * tests the compare method when the two objects
      * have different titles
      */
-    public void testCompareDiffTitle() {
+    public void testCompareDiffTitle()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hello", "Michael Jackson", 1997, "rock", 2);
-        assertTrue(comparer.compare(song1, song2) != 0);
+        assertNotSame(0, comparer.compare(song1, song2));
     }
-
-
+    
     /**
      * tests the compare method when the two objects
      * have the same title
      */
-    public void testCompareSameTitle() {
+    public void testCompareSameTitle()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hey", "Michael Jackson", 1997, "pop", 2);
-        assertTrue(comparer.compare(song1, song2) == 0);
+        assertEquals(0, comparer.compare(song1, song2));
     }
-
+    
 }

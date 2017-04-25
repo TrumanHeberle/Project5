7c040c65e2329808387fe121815560000aa9d478
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:54:07 -0400

    Update CompareSongArtistTest.java

--- Project5/src/prj5/CompareSongArtistTest.java
+++ Project5/src/prj5/CompareSongArtistTest.java
@@ -1,43 +1,44 @@
+/**
+ * 
+ */
 package prj5;
 
 /**
- * Tests the CompareSongArtist class
- * 
  * @author AshlynUser
- * @version 4.19.2017
+ * @version 2017.04.19
  */
 public class CompareSongArtistTest extends student.TestCase {
 
     private CompareSongArtist comparer;
-
-
+    
     /**
      * sets up test methods
      */
-    public void setUp() {
+    public void setUp()
+    {
         comparer = new CompareSongArtist();
     }
-
-
+    
     /**
      * tests the compare method when the two objects
      * have different artists
      */
-    public void testCompareDiffArtists() {
+    public void testCompareDiffArtists()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hello", "Michael Jackson", 1997, "rock", 2);
-        assertTrue(comparer.compare(song1, song2) != 0);
+        assertNotSame(0, comparer.compare(song1, song2));
     }
-
-
+    
     /**
      * tests the compare method when the two objects
-     * have the same artist
+     * have different artists
      */
-    public void testCompareSameArtist() {
+    public void testCompareSameArtist()
+    {
         Song song1 = new Song("hey", "Taylor Swift", 1998, "pop", 1);
         Song song2 = new Song("hello", "Taylor Swift", 1997, "rock", 2);
-        assertTrue(comparer.compare(song1, song2) == 0);
+        assertEquals(0, comparer.compare(song1, song2));
     }
-
+    
 }

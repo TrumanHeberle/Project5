c7aa8b3f4a1d146cb14daa602d0281fca82fb4f9
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:59:55 -0400

    Update LinkedSongListTest.java

--- Project5/src/prj5/LinkedSongListTest.java
+++ Project5/src/prj5/LinkedSongListTest.java
@@ -1,89 +1,109 @@
 package prj5;
-import java.io.FileNotFoundException;
+
 import java.util.Arrays;
 import student.TestCase;
 
+/**
+ * Tests the PersonScanner class
+ * 
+ * @author Truman Heberle trumanh
+ * @version 4.19.2017
+ */
+public class LinkedSongListTest extends TestCase {
+
+    private Song song1; 
+    private Song song2;
+    private Song song3;
+
+
     /**
-     * Tests the PersonScanner class
-     * 
-     * @author Truman Heberle trumanh
-     * @version 4.19.2017
+     * Runs before each test case
+     *
+    public void setUp() {
+
+    }*/
+
+
+    /**
+     * Tests the sortByTitle method
      */
-    public class LinkedSongListTest extends TestCase {
-        private LinkedSongList<Song> list;
-        private Song song1;
-        private Song song2;
-        private Song song3;
-
-
-        /**
-         * Runs before each test case
-         */
-        public void setUp() {
-            list = new LinkedSongList<Song>();
-            song1 = new Song("Watching the Detectives", "Elvis Costello", 1977, "punk", 1);
-            song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
-            song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
-            list.add(0, song1);
-            list.add(1, song2);
-            list.add(2, song3);
-        }
-
-        /**
-         * Tests the sortByTitle method
-         */
-        public void testSortByTitle() {
-            LinkedSongList<Song> list1 = new LinkedSongList<Song>();
-            song1 = new Song("Watching the Detectives", "Elvis Costello", 1977, "punk", 1);
-            song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
-            song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
-            list1.add(0, song3);
-            list1.add(1, song2);
-            list1.add(2, song1);
-            assertEquals(Arrays.toString(list.sortByTitle().toArray()), Arrays.toString(list1.toArray()));
-        }
-        
-        /**
-         * Tests the sortByArtist method
-         */
-        public void testSortByArtist() {
-            LinkedSongList<Song> list1 = new LinkedSongList<Song>();
-            song1 = new Song("Watching the Detectives", "Elvis Costello", 1977, "punk", 1);
-            song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
-            song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
-            list1.add(0, song2);
-            list1.add(1, song1);
-            list1.add(2, song3);
-            assertEquals(Arrays.toString(list.sortByArtist().toArray()), Arrays.toString(list1.toArray()));
-        }
-        
-        /**
-         * Tests the sortByYear method
-         */
-        public void testSortByYear() {
-            LinkedSongList<Song> list1 = new LinkedSongList<Song>();
-            song1 = new Song("Watching the Detectives", "Elvis Costello", 1977, "punk", 1);
-            song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
-            song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
-            list1.add(0, song1);
-            list1.add(1, song2);
-            list1.add(2, song3);
-            assertEquals(Arrays.toString(list.sortByYear().toArray()), Arrays.toString(list1.toArray()));
-        }
-        
-        /**
-         * Tests the sortByGenre method
-         */
-        public void testSortByGenre() {
-            LinkedSongList<Song> list1 = new LinkedSongList<Song>();
-            song1 = new Song("Watching the Detectives", "Elvis Costello", 1977, "punk", 1);
-            song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
-            song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
-            list1.add(0, song2);
-            list1.add(1, song1);
-            list1.add(2, song3);
-            assertEquals(Arrays.toString(list.sortByGenre().toArray()), Arrays.toString(list1.toArray()));
-        }
+    public void testSortByTitle() {
+        LinkedSongList<Song> list1 = new LinkedSongList<Song>();
+        LinkedSongList<Song> list2 = new LinkedSongList<Song>();
+        song2 = new Song("Watching the Detectives", "Elvis Costello", 1977,
+            "punk", 1);
+        song1 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
+        song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
+        list1.add(0, song3);
+        list1.add(1, song1);
+        list1.add(2, song2);
+        list2.add(0, song1);
+        list2.add(1, song2);
+        list2.add(2, song3);
+        assertEquals(Arrays.toString(list1.toArray()), Arrays.toString(list2
+            .sortByTitle().toArray()));
     }
 
 
+    /**
+     * Tests the sortByArtist method
+     */
+    public void testSortByArtist() {
+        LinkedSongList<Song> list1 = new LinkedSongList<Song>();
+        LinkedSongList<Song> list2 = new LinkedSongList<Song>();
+        song1 = new Song("Watching the Detectives", "Elvis Costello", 1977,
+            "punk", 1);
+        song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
+        song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
+        list1.add(0, song2);
+        list1.add(1, song1);
+        list1.add(2, song3);
+        list2.add(0, song1);
+        list2.add(1, song2);
+        list2.add(2, song3);
+        assertEquals(Arrays.toString(list1.toArray()), Arrays.toString(list2
+            .sortByArtist().toArray()));
+    }
+
+
+    /**
+     * Tests the sortByYear method
+     */
+    public void testSortByYear() {
+        LinkedSongList<Song> list1 = new LinkedSongList<Song>();
+        LinkedSongList<Song> list2 = new LinkedSongList<Song>();
+        song1 = new Song("Watching the Detectives", "Elvis Costello", 1977,
+            "punk", 1);
+        song2 = new Song("Upside Down", "Diana Ross", 2017, "disco", 2);
+        song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
+        list1.add(0, song1);
+        list1.add(1, song3);
+        list1.add(2, song2);
+        list2.add(0, song1);
+        list2.add(1, song2);
+        list2.add(2, song3);
+        assertEquals(Arrays.toString(list1.toArray()), Arrays.toString(list2
+            .sortByYear().toArray()));
+    }
+
+
+    /**
+     * Tests the sortByGenre method
+     */
+    public void testSortByGenre() {
+        LinkedSongList<Song> list1 = new LinkedSongList<Song>();
+        LinkedSongList<Song> list2 = new LinkedSongList<Song>();
+        song1 = new Song("Watching the Detectives", "Elvis Costello", 1977,
+            "punk", 1);
+        song2 = new Song("Upside Down", "Diana Ross", 1980, "disco", 2);
+        song3 = new Song("Uma Thurman", "Fall Out Boy", 2015, "rock", 3);
+        list1.add(0, song2);
+        list1.add(1, song1);
+        list1.add(2, song3);
+        list2.add(0, song1);
+        list2.add(1, song2);
+        list2.add(2, song3);
+        assertEquals(Arrays.toString(list1.toArray()), Arrays.toString(list2
+            .sortByGenre().toArray()));
+    }
+}

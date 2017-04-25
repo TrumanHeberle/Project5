142a2377b2e1b2b01e533c9e9c09af7791459aa5
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:53:50 -0400

    Update CompareSongArtist.java

--- Project5/src/prj5/CompareSongArtist.java
+++ Project5/src/prj5/CompareSongArtist.java
@@ -1,14 +1,16 @@
+/**
+ * 
+ */
 package prj5;
 
 import java.util.Comparator;
 
 /**
- * Comparator for Song Artists
- * 
  * @author AshlynUser
- * @version 4.19.2017
+ * @version 2017.04.19
  */
 public class CompareSongArtist implements Comparator<Object> {
+    
     /**
      * compares two songs based on the artist
      */
@@ -17,4 +19,5 @@ public class CompareSongArtist implements Comparator<Object> {
     {
         return ((Song)o1).getArtist().compareTo(((Song)o2).getArtist());
     }
+
 }

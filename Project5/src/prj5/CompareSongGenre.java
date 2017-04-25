d743b76f65b0538ec875b20cba63d7ae2978358b
mashlyn6 <mashlyn6@vt.edu>
2017-04-25 18:54:59 -0400

    Update CompareSongGenre.java

--- Project5/src/prj5/CompareSongGenre.java
+++ Project5/src/prj5/CompareSongGenre.java
@@ -1,19 +1,23 @@
+
 package prj5;
 
 import java.util.Comparator;
 
 /**
- * Comparator for Song Genres
- * 
  * @author AshlynUser
- * @version 4.19.2017
+ * @version 2017.04.19
  */
 public class CompareSongGenre implements Comparator<Object> {
+
     /**
      * compares two songs based on the genre
+     * @param o1
+     * @param o2
+     * @return 
      */
     @Override
-    public int compare(Object o1, Object o2) {
+    public int compare(Object o1, Object o2)
+    {
         return ((Song)o1).getGenre().compareTo(((Song)o2).getGenre());
     }
 }

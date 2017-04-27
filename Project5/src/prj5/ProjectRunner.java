eca85f35e82d72bc43c858a2f2972ab8b56db729
mashlyn6 <mashlyn6@vt.edu>
2017-04-26 21:02:04 -0400

    Update ProjectRunner.java

--- Project5/src/prj5/ProjectRunner.java
+++ Project5/src/prj5/ProjectRunner.java
@@ -2,6 +2,7 @@
 package prj5;
 
 import java.io.FileNotFoundException;
+
 import java.util.Iterator;
 
 /**
@@ -18,68 +19,23 @@ public class ProjectRunner {
      * @throws FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
-        //PersonScanner personScanner = new PersonScanner(args[0]);
-        //DoublyLinkedList<Person> people = personScanner.getPeopleList();
-        //SongScanner songScanner = new SongScanner(args[1]);
-        //LinkedSongList<Song> songs = songScanner.getSongList().sortByGenre();// .sortByTitle();
-        //LinkedSongList<Song> titleSongs = songScanner.getSongList()
-          //  .sortByTitle();
-        GUIDisplayWindow gui = new GUIDisplayWindow();
-
-       /** for (Song song : songs) {
-            // System.out.println(songs.size());
-            System.out.println("Song Title: " + song.getTitle());
-            System.out.println("Song Artist: " + song.getArtist());
-            System.out.println("Song Genre: " + song.getGenre());
-            System.out.println("Song Year: " + song.getYear());
-            System.out.println("Heard");
-            System.out.print("reading:" + song.getHobbyScore(HobbyEnum.READ,
-                people)[0]);
-            System.out.print(" art:" + song.getHobbyScore(HobbyEnum.ART,
-                people)[0]);
-            System.out.print(" sports:" + song.getHobbyScore(HobbyEnum.SPORTS,
-                people)[0]);
-            System.out.println(" music:" + song.getHobbyScore(HobbyEnum.MUSIC,
-                people)[0]);
-            System.out.println("Likes");
-            System.out.print("reading:" + song.getHobbyScore(HobbyEnum.READ,
-                people)[1]);
-            System.out.print(" art:" + song.getHobbyScore(HobbyEnum.ART,
-                people)[1]);
-            System.out.print(" sports:" + song.getHobbyScore(HobbyEnum.SPORTS,
-                people)[1]);
-            System.out.println(" music:" + song.getHobbyScore(HobbyEnum.MUSIC,
-                people)[1]);
-            System.out.println("");
+        if (args.length == 2)
+        {
+            PersonScanner personScanner = new PersonScanner(args[0]);
+            DoublyLinkedList<Person> people = personScanner.getPeopleList();
+            SongScanner songScanner = new SongScanner(args[1]);
+            LinkedSongList<Song> songs = songScanner.getSongList();
+            GUIDisplayWindow gui = new GUIDisplayWindow(people, songs);
         }
-
-        for (Song song : titleSongs) {
-            // System.out.println(songs.size());
-            System.out.println("Song Title: " + song.getTitle());
-            System.out.println("Song Artist: " + song.getArtist());
-            System.out.println("Song Genre: " + song.getGenre());
-            System.out.println("Song Year: " + song.getYear());
-            System.out.println("Heard");
-            System.out.print("reading:" + song.getHobbyScore(HobbyEnum.READ,
-                people)[0]);
-            System.out.print(" art:" + song.getHobbyScore(HobbyEnum.ART,
-                people)[0]);
-            System.out.print(" sports:" + song.getHobbyScore(HobbyEnum.SPORTS,
-                people)[0]);
-            System.out.println(" music:" + song.getHobbyScore(HobbyEnum.MUSIC,
-                people)[0]);
-            System.out.println("Likes");
-            System.out.print("reading:" + song.getHobbyScore(HobbyEnum.READ,
-                people)[1]);
-            System.out.print(" art:" + song.getHobbyScore(HobbyEnum.ART,
-                people)[1]);
-            System.out.print(" sports:" + song.getHobbyScore(HobbyEnum.SPORTS,
-                people)[1]);
-            System.out.println(" music:" + song.getHobbyScore(HobbyEnum.MUSIC,
-                people)[1]);
-            System.out.println("");
-        }*/
-
+        else
+        {
+            PersonScanner personScanner = new PersonScanner("MusicSurveyData2017S.csv");
+            DoublyLinkedList<Person> people = personScanner.getPeopleList();
+            SongScanner songScanner = new SongScanner("SongList2017S.csv ");
+            LinkedSongList<Song> songs = songScanner.getSongList();
+            GUIDisplayWindow gui = new GUIDisplayWindow(people, songs);
+        }
+       
     }
 
 }

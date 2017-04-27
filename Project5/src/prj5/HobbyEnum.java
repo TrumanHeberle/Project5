5e274805f3bf12dc2352530e6eb5499757ba0598
mashlyn6 <mashlyn6@vt.edu>
2017-04-27 00:40:44 -0400

    Update HobbyEnum.java

--- Project5/src/prj5/HobbyEnum.java
+++ Project5/src/prj5/HobbyEnum.java
@@ -1,14 +1,20 @@
+/**
+ * 
+ */
 package prj5;
 
 /**
- * Contains all types of Hobbies
- * @author Truman Heberle trumanh
- * @version 4.15.2017
+ * @author Ashlyn McDonald (mashlyn6)
+ * @author Truman Heberle (trumanh)
+ * @author Shivani Garg (shivanig)
+ * @version 2017.04.26
+ * 
+ *          This enumerator contains the four
+ *          possibilities for a person's hobby:
+ *          reading, art, sports, and music.
+ *
  */
 public enum HobbyEnum {
-    READ,
-    ART,
-    SPORTS,
-    MUSIC,
-    ELSE;
+
+    READ, ART, SPORTS, MUSIC;
 }

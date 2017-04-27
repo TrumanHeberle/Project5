160c0188cc42410891392270822413f955b377b1
mashlyn6 <mashlyn6@vt.edu>
2017-04-27 00:38:07 -0400

    Update CompareSongYear.java

--- Project5/src/prj5/CompareSongYear.java
+++ Project5/src/prj5/CompareSongYear.java
@@ -6,13 +6,23 @@ package prj5;
 import java.util.Comparator;
 
 /**
- * @author AshlynUser
- * @version 2017.04.19
+ * @author Ashlyn McDonald (mashlyn6)
+ * @author Truman Heberle (trumanh)
+ * @author Shivani Garg (shivanig)
+ * @version 2017.04.26
+ * 
+ * This class compares two songs by the song year.
  */
 public class CompareSongYear implements Comparator<Object> {
-    
+     
     /**
-     * compares two songs based on the year
+     * Compares two songs based on the song's year
+     * @param o1 the first object
+     * @param o2 the second object
+     * @return the integer value that represents whether
+     * the first song's year comes before or after the second
+     * one's year (0 if they are the same, the difference in 
+     * years if they are not)
      */
     @Override
     public int compare(Object o1, Object o2)

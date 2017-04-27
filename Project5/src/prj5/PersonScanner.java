3a24fcf14aad51ce044ff2cd22bf37c4be5c6712
mashlyn6 <mashlyn6@vt.edu>
2017-04-27 00:42:15 -0400

    Update PersonScanner.java

--- Project5/src/prj5/PersonScanner.java
+++ Project5/src/prj5/PersonScanner.java
@@ -5,16 +5,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
+ * @author Ashlyn McDonald (mashlyn6)
+ * @author Truman Heberle (trumanh)
+ * @author Shivani Garg (shivanig)
+ * @version 2017.04.26
  * 
- * @author AshlynUser
- * @version 2017.04.19
+ *          This class scans a file of people
+ *          and their survey responses and then
+ *          creates a list of the people to be used
+ *          in other aspects of this project.
  */
 public class PersonScanner {
     private DoublyLinkedList<Person> peopleList;
 
 
     /**
-     * the scanner constructor
+     * the scanner constructor, which initializes
+     * the DoublyLinkedList of people
      */
     public PersonScanner() {
         peopleList = new DoublyLinkedList<Person>();
@@ -22,11 +29,14 @@ public class PersonScanner {
 
 
     /**
-     * constructor with a parameter
+     * the constructor with a parameter, which
+     * creates a new scanner out of the file and initializes
+     * the field
      * 
      * @param fileName
      *            the name of the file
      * @throws FileNotFoundException
+     *             if the file is invalid
      */
     public PersonScanner(String fileName) throws FileNotFoundException {
         peopleList = new DoublyLinkedList<Person>();
@@ -35,32 +45,28 @@ public class PersonScanner {
 
 
     /**
-     * scans the file of people
+     * scans the file of people and adds them
+     * to the list that was created as a field
      * 
      * @param fileName
      *            the file name
-     * @return the list
+     * @return the list of people created from the file
      * @throws FileNotFoundException
+     *             if the file is invalid
      */
     public DoublyLinkedList<Person> scanPeople(String fileName)
         throws FileNotFoundException {
         try {
             Scanner scan = new Scanner(new File(fileName));
-            // Skips Header
-            // if (scan.hasNextLine()) {
             scan.nextLine();
-            // }
+            scan.nextLine();
 
-            // Searches through list
             while (scan.hasNextLine()) {
                 String line = scan.nextLine();
                 if (line.endsWith(",")) {
                     line = line + "n/a";
                 }
                 String[] values = line.split(" *, *");
-                // for (int i = 0; i < values.length; i++) {
-                // values[i] = values[i].trim();
-                // }
                 Person person = new Person();
                 person.setId(Integer.parseInt(values[0]));
                 person.setDate(values[1]);
@@ -73,12 +79,7 @@ public class PersonScanner {
                     person = null;
                 }
                 if (values[3].contains("Southeast") || values[3].contains(
-                    "Northeast") || values[3].contains("Outside"))// ||
-                                                                  // values[3]
-                // .contains("than")) {
-                // took out above line because testing wasn't
-                // working (something with the word- "rest"?)
-                {
+                    "Northeast") || values[3].contains("Outside")) {
                     person.setRegion(values[3]);
                 }
                 else {
@@ -92,8 +93,6 @@ public class PersonScanner {
                 else {
                     person = null;
                 }
-                // String[] answers = Arrays.copyOfRange(values, 4,
-                // values.length);
                 String[] answers = new String[values.length - 5];
                 for (int i = 0; i <= values.length - 6; i++) {
                     answers[i] = values[i + 5];
@@ -105,18 +104,15 @@ public class PersonScanner {
                     person.setAnswers(answers);
                     peopleList.addToBack(person);
                 }
-                // return peopleList;
 
             }
 
             scan.close();
         }
         catch (FileNotFoundException e) {
-            // System.out.println("File name is invalid");
             e.getMessage();
         }
         return peopleList;
-        // scan.close();
     }
 
 
